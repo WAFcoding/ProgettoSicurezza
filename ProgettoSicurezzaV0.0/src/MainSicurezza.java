69ba09142691f6eb31a546f9d4aab316f163800f
WAFcoding <pasquale.verlotta@gmail.com>
2014-07-17 15:31:12 +0200

    aggiunto esempio di gridbaglayout

--- ProgettoSicurezzaV0.0/src/MainSicurezza.java
+++ ProgettoSicurezzaV0.0/src/MainSicurezza.java
@@ -9,6 +9,12 @@ public class MainSicurezza {
 	public static void main(String[] args) {
 		ImageMagick img;
 
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	PrimaryLayout p= new PrimaryLayout();
+                p.createAndShowGUI();
+            }
+        });
 	}
 
 }
