a2824d2c8e353cd55af3dd5013d08dbbf3ee15df
Giovanni Rossi <ziogio91@gmail.com>
2014-07-23 16:03:10 +0200

    aggiunta classe di utilità per KeyStore

--- ProgettoSicurezzaV0.0/src/test/TestCrypto.java
+++ ProgettoSicurezzaV0.0/src/test/TestCrypto.java
@@ -1,10 +1,12 @@
 package test;
 
 import java.math.BigInteger;
+import java.security.KeyPair;
 
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 
 import util.CryptoUtility;
+import util.KeyStoreFacility;
 
 
 public class TestCrypto {
@@ -34,11 +36,28 @@ public class TestCrypto {
 		System.out.println(CryptoUtility.hash(CryptoUtility.HASH_ALGO.SHA256, testData));
 		System.out.println(CryptoUtility.hash(CryptoUtility.HASH_ALGO.SHA512, testData));
 		
-		System.out.println("\nGENERAZIONE COPPIA CHIAVI");
-		AsymmetricCipherKeyPair keys = CryptoUtility.getKeyPair();
-		BigInteger[] signature = CryptoUtility.sign(keys.getPrivate(), testData);
-		System.out.println("Verifica Firma TESTO CORRETTO:" + CryptoUtility.verify(keys.getPublic(), signature, testData));
-		System.out.println("Verifica Firma TESTO SABOTATO:" + CryptoUtility.verify(keys.getPublic(), signature, testData + "x"));
+		System.out.println("\nGENERAZIONE COPPIA CHIAVI(DSA)");
+		AsymmetricCipherKeyPair keys = CryptoUtility.genDSAKeyPair();
+		BigInteger[] signature = CryptoUtility.signDSA(keys.getPrivate(), testData);
+		System.out.println("Verifica DSA Firma TESTO CORRETTO:" + CryptoUtility.verifyDSA(keys.getPublic(), signature, testData));
+		System.out.println("Verifica DSA Firma TESTO SABOTATO:" + CryptoUtility.verifyDSA(keys.getPublic(), signature, testData + "x"));
+		
+		System.out.println("\nGENERAZIONE COPPIA CHIAVI(RSA)");
+		KeyPair keyps = CryptoUtility.genKeyPairRSA();
+		byte[] signaturersa = CryptoUtility.signRSA(keyps.getPrivate(), testData);
+		System.out.println("Verifica RSA Firma TESTO CORRETTO:" + CryptoUtility.verifyRSA(keyps.getPublic(), signaturersa, testData));
+		System.out.println("Verifica RSA Firma TESTO SABOTATO:" + CryptoUtility.verifyRSA(keyps.getPublic(), signaturersa, testData + "x"));
+		
+		System.out.println("\nTEST-KEYSTORE");
+		KeyStoreFacility keystore = KeyStoreFacility.getInstance();
+		keystore.saveKey("giovanni", key);
+		System.out.println(keystore.getKey("giovanni"));
+		keystore.saveChanges();
+		
+		//FIXME
+		keystore.savePrivateKey("giovanni", keyps.getPrivate());
+		System.out.println(keystore.getPrivateKey("giovanni").toString());
+		keystore.saveChanges();
 		
 	}
 
--- ProgettoSicurezzaV0.0/src/util/CryptoUtility.java
+++ ProgettoSicurezzaV0.0/src/util/CryptoUtility.java
@@ -2,9 +2,18 @@ package util;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.security.Signature;
+import java.security.SignatureException;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -23,26 +32,61 @@ import org.bouncycastle.util.Arrays;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
+/**
+ * Classe di utilità per l'utilizzo degli algoritmi di crittografia.
+ * 
+ * @author Giovanni Rossi
+ *
+ */
 public class CryptoUtility {
 
+	/**
+	 *	Provide per la libreria Bouncy Castle. 
+	 */
 	private static final String BouncyProvider = "BC";
 
+	/**
+	 * Permette di stabilire il tipo di algoritmo di cifratura da usare.
+	 */
 	public enum CRYPTO_ALGO {
 		AES, DES
 	};
 
+	/**
+	 * Permette di stabilire il tipo di algoritmo di hashing da usare.
+	 */
 	public enum HASH_ALGO {
 		MD5, SHA1, SHA256, SHA512
 	};
 
+	/**
+	 * Converte i dati in input in Base64.
+	 * @param data	I dati da convertire.
+	 * 
+	 * @return I dati in formato Base64.	
+	 */
 	public static String toBase64(byte[] data) {
 		return new BASE64Encoder().encode(data);
 	}
 
+	/**
+	 * Decodifica i dati dal Base64.
+	 * @param data	I dati da decodificare.
+	 * 
+	 * @return	I dati decodificati.
+	 * 
+	 * @throws IOException
+	 */
 	public static byte[] fromBase64(String data) throws IOException {
 		return new BASE64Decoder().decodeBuffer(data);
 	}
 
+	/**
+	 * Restituisce la stringa con il tipo di algoritmo di cifratura da usare.
+	 * @param algo	Il tipo di algoritmo.
+	 * 
+	 * @return	La stringa corrispondente al tipo di algoritmo da usare.
+	 */
 	private static String getCipher(CRYPTO_ALGO algo) {
 		switch (algo) {
 		case AES:
@@ -54,6 +98,12 @@ public class CryptoUtility {
 		}
 	}
 
+	/**
+	 * Restituisce la stringa con il tipo di algoritmo di hashing da usare.
+	 * @param algo	Il tipo di algoritmo.
+	 * 
+	 * @return	La stringa corrispondente al tipo di algoritmo da usare.
+	 */
 	private static String getHash(HASH_ALGO algo) {
 		switch (algo) {
 		case MD5:
@@ -69,6 +119,13 @@ public class CryptoUtility {
 		}
 	}
 
+	/**
+	 * Pre processa la chiave in input per adattarla all'algoritmo scelto.
+	 * @param algo	L'algoritmo di cifratura scelto.
+	 * @param key	La chiave da manipolare.
+	 * 
+	 * @return La chiave processata.
+	 */
 	private static byte[] preProcess(CRYPTO_ALGO algo, byte[] key) {
 		switch (algo) {
 		case AES:
@@ -80,6 +137,12 @@ public class CryptoUtility {
 		}
 	}
 
+	/**
+	 * Converte i byte in ingresso in una stringa esadecimale.
+	 * @param b		I byte da convertire.
+	 * 
+	 * @return La stringa convertita in formato esadecimale.
+	 */
 	private static String bytesToHex(byte[] b) {
 		char hexDigit[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 				'A', 'B', 'C', 'D', 'E', 'F' };
@@ -91,16 +154,46 @@ public class CryptoUtility {
 		return buf.toString();
 	}
 
+	/**
+	 * Cifra la stringa in input con la chiave e l'algoritmo scelto.
+	 * @param algo	L'algoritmo di cifratura.
+	 * @param data	La stringa da cifrare.
+	 * @param key	La chiave da usare nel processo.
+	 * 
+	 * @return I dati cifrati.
+	 * 
+	 * @throws Exception
+	 */
 	public static byte[] encrypt(CRYPTO_ALGO algo, String data, String key)
 			throws Exception {
 		return encrypt(algo, data.getBytes(), key.getBytes());
 	}
 
+	/**
+	 * Cifra i dati in input con la chiave e l'algoritmo scelto.
+	 * @param algo	L'algoritmo di cifratura.
+	 * @param data	I dati da cifrare.
+	 * @param key	La chiave da usare nel processo.
+	 * 
+	 * @return I dati cifrati.
+	 * 
+	 * @throws Exception
+	 */
 	public static byte[] encrypt(CRYPTO_ALGO algo, byte[] data, String key)
 			throws Exception {
 		return encrypt(algo, data, key.getBytes());
 	}
 
+	/**
+	 * Cifra i dati in input con la chiave e l'algoritmo scelto.
+	 * @param algo	L'algoritmo di cifratura.
+	 * @param data	I dati da cifrare.
+	 * @param key	La chiave da usare nel processo.
+	 * 
+	 * @return I dati cifrati.
+	 * 
+	 * @throws Exception
+	 */
 	public static byte[] encrypt(CRYPTO_ALGO algo, byte[] data, byte[] key)
 			throws Exception {
 		Security.addProvider(new BouncyCastleProvider());
@@ -114,6 +207,16 @@ public class CryptoUtility {
 		return c.doFinal(data);
 	}
 
+	/**
+	 * Decifra i dati in input con la chiave e l'algoritmo scelto.
+	 * @param algo	L'algoritmo di decifratura.
+	 * @param data	I dati da decifrare.
+	 * @param key	La chiave da usare nel processo.
+	 * 
+	 * @return I dati decifrati.
+	 * 
+	 * @throws Exception
+	 */
 	public static byte[] decrypt(CRYPTO_ALGO algo, byte[] data, byte[] key)
 			throws Exception {
 		Security.addProvider(new BouncyCastleProvider());
@@ -127,15 +230,43 @@ public class CryptoUtility {
 		return c.doFinal(data);
 	}
 
+	/**
+	 * Decifra i dati in input con la chiave e l'algoritmo scelto.
+	 * @param algo	L'algoritmo di decifratura.
+	 * @param data	I dati da decifrare.
+	 * @param key	La chiave da usare nel processo.
+	 * 
+	 * @return I dati decifrati.
+	 * 
+	 * @throws Exception
+	 */
 	public static String decrypt(CRYPTO_ALGO algo, byte[] data, String key)
 			throws Exception {
 		return new String(decrypt(algo, data, key.getBytes()));
 	}
 
+	/**
+	 * Calcola l'hash della stringa in input seguendo l'algoritmo indicato.
+	 * @param algo	L'algoritmo di hashing da usare.
+	 * @param data	La stringa di cui calcolare l'hash.
+	 * 
+	 * @return La stringa rappresentante l'hash in formato esadecimale.
+	 * 
+	 * @throws Exception
+	 */
 	public static String hash(HASH_ALGO algo, String data) throws Exception {
 		return hash(algo, data.getBytes());
 	}
 
+	/**
+	 * Calcola l'hash dei dati in input seguendo l'algoritmo indicato.
+	 * @param algo	L'algoritmo di hashing da usare.
+	 * @param data	I dati di cui calcolare l'hash.
+	 * 
+	 * @return La stringa rappresentante l'hash in formato esadecimale.
+	 * 
+	 * @throws Exception
+	 */
 	public static String hash(HASH_ALGO algo, byte[] data) throws Exception {
 		Security.addProvider(new BouncyCastleProvider());
 
@@ -145,35 +276,124 @@ public class CryptoUtility {
 		return bytesToHex(md.digest(data));
 	}
 
-	public static AsymmetricCipherKeyPair getKeyPair() {
-		SecureRandom    random = new SecureRandom();
+	/**
+	 * Genera una coppia di chiavi asimmetriche RSA.
+	 * 
+	 * @return La coppia di chiavi.
+	 * 
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchProviderException
+	 */
+	public static KeyPair genKeyPairRSA() throws NoSuchAlgorithmException,
+			NoSuchProviderException {
+		Security.addProvider(new BouncyCastleProvider());
 
-        DSAParametersGenerator  pGen = new DSAParametersGenerator();
-        pGen.init(512, 80, random);
+		KeyPairGenerator g = KeyPairGenerator.getInstance("RSA", BouncyProvider);
 
-        DSAParameters params = pGen.generateParameters();
+		g.initialize(1024, new SecureRandom());
+
+		return g.generateKeyPair();
+	}
+	
+	/**
+	 * Firma i dati passati in input usando l'algoritmo RSA ("SHA1withRSA").
+	 * @param pkey	La chiave privata.
+	 * @param data	I dati da firmare.
+	 * 
+	 * @return I dati firmati con la chiave privata.
+	 * 
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchProviderException
+	 * @throws InvalidKeyException
+	 * @throws SignatureException
+	 */
+	public static byte[] signRSA(PrivateKey pkey, String data) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
+		Security.addProvider(new BouncyCastleProvider());
+		Signature sign = Signature.getInstance("SHA1withRSA", BouncyProvider);
+		sign.initSign(pkey, new SecureRandom());
+		sign.update(data.getBytes());
+		return sign.sign();
+	}
+	
+	/**
+	 * Verifica la firma usando RSA ("SHA1withRSA").
+	 * @param pkey		La chiave pubblica.
+	 * @param data		I dati firmati.
+	 * @param message	Il messaggio da verificare.
+	 * 
+	 * @return true se la stringa non ha subito manipolazione e false altrimenti.
+	 * 
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchProviderException
+	 * @throws InvalidKeyException
+	 * @throws SignatureException
+	 */
+	public static boolean verifyRSA(PublicKey pkey, byte[] data, String message) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
+		Security.addProvider(new BouncyCastleProvider());
+		Signature sign = Signature.getInstance("SHA1withRSA", BouncyProvider);
+		sign.initVerify(pkey);
+		sign.update(message.getBytes());
+		return sign.verify(data);
+	}
+
+	/**
+	 * Genera una coppia di chiavi DSA (Digital Signature Algorithm).
+	 * 
+	 * @return La coppia di chiavi.
+	 */
+	public static AsymmetricCipherKeyPair genDSAKeyPair() {
+		SecureRandom random = new SecureRandom();
+
+		DSAParametersGenerator pGen = new DSAParametersGenerator();
+		pGen.init(512, 80, random);
+
+		DSAParameters params = pGen.generateParameters();
 		DSAKeyPairGenerator dsaKeyGen = new DSAKeyPairGenerator();
-		DSAKeyGenerationParameters genParam = new DSAKeyGenerationParameters(random, params);
+		DSAKeyGenerationParameters genParam = new DSAKeyGenerationParameters(
+				random, params);
 
 		dsaKeyGen.init(genParam);
 
 		return dsaKeyGen.generateKeyPair();
 	}
-	
-	public static BigInteger[] sign(AsymmetricKeyParameter privateKey, String message) throws Exception {
-		SecureRandom    random = new SecureRandom();
+
+	/**
+	 * Firma il messaggio in input con DSA.
+	 * @param privateKey	La chiave privata.
+	 * @param message		Il messaggio da firmare.
+	 * 
+	 * @return	La firma dei dati.
+	 * 
+	 * @throws Exception
+	 */
+	public static BigInteger[] signDSA(AsymmetricKeyParameter privateKey,
+			String message) throws Exception {
+		SecureRandom random = new SecureRandom();
 		DSASigner signer = new DSASigner();
-		
-		ParametersWithRandom param = new ParametersWithRandom(privateKey, random);
+
+		ParametersWithRandom param = new ParametersWithRandom(privateKey,
+				random);
 
 		signer.init(true, param);
-		return signer.generateSignature(hash(HASH_ALGO.SHA1,message).getBytes());
+		return signer.generateSignature(hash(HASH_ALGO.SHA1, message)
+				.getBytes());
 	}
-	
-	public static boolean verify(AsymmetricKeyParameter publicKey, BigInteger[] signature, String message) throws Exception {
+
+	/**
+	 * Verifica la firma del messaggio in input usando DSA.
+	 * @param publicKey		La chiave pubblica.
+	 * @param signature		La firma del messaggio.
+	 * @param message		Il messaggio da verificare.
+	 * 
+	 * @return true se il messaggio non ha subito manipolazioni e false altrimenti.
+	 * @throws Exception
+	 */
+	public static boolean verifyDSA(AsymmetricKeyParameter publicKey,
+			BigInteger[] signature, String message) throws Exception {
 		DSASigner signer = new DSASigner();
 		signer.init(false, publicKey);
-		return signer.verifySignature(hash(HASH_ALGO.SHA1,message).getBytes(), signature[0], signature[1]);
+		return signer.verifySignature(hash(HASH_ALGO.SHA1, message).getBytes(),
+				signature[0], signature[1]);
 	}
 
 }
