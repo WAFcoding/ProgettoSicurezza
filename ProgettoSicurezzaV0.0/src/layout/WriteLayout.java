70417ac5c9e6997a036f25ebb0d44b81cd35136a
pasquale <pasquale.verlotta@gmail.com>
2015-02-08 12:56:34 +0100

    finita implementazione della creazione del pdf, sistemato le funzioni del write layout

--- ProgettoSicurezzaV0.0/src/layout/WriteLayout.java
+++ ProgettoSicurezzaV0.0/src/layout/WriteLayout.java
@@ -6,7 +6,6 @@ package layout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -22,12 +21,12 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-
+import usersManagement.User;
 import util.CryptoUtility;
 import util.PDFUtil;
 import util.QRCode;
+
+import com.itextpdf.text.DocumentException;
 
 /**
  * @author "Pasquale Verlotta - pasquale.verlotta@gmail.com"
@@ -45,6 +44,7 @@ public class WriteLayout implements GeneralLayout{
 	private File currentFile;
 	private boolean isAlreadyConfigured;
 	private boolean receiverSetted;
+	private User user;
 
 	public WriteLayout(LayoutControl control, Container pane){
 		setControl(control);
@@ -53,6 +53,8 @@ public class WriteLayout implements GeneralLayout{
 		area.setVisible(true);
 		this.isAlreadyConfigured= false;
 		this.receiverSetted= false;
+		
+		this.user= control.getUser_manager().getActualUser();
 	}
 	
 	@Override
@@ -104,27 +106,7 @@ public class WriteLayout implements GeneralLayout{
 		c.gridx= posx;c.gridy= posy;
 		button.setBackground(Color.BLUE);
 		button.setForeground(Color.WHITE);
-		button.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String[] options= {"Utente specifico", "Gruppo di utenti"};
-				int choice= JOptionPane.showOptionDialog(pane, "Come criptare il documento", "Attenzione", JOptionPane.YES_NO_OPTION, 
-						JOptionPane.QUESTION_MESSAGE, null, options, "Seleziona");
-				//utente specifico
-				if(choice == JOptionPane.YES_OPTION){
-					setReceiverSetted(true);
-					control.setLayout("RECEIVER");
-					control.setReceiverSingleUSer(true);
-				}
-				//gruppo di utenti
-				else if(choice == JOptionPane.NO_OPTION){
-					setReceiverSetted(true);
-					control.setLayout("RECEIVER");
-					control.setReceiverSingleUSer(false);
-				}
-			}
-		});
+		button.addActionListener(new SelectReceiverAction());
 		pane.add(button, c);
 		
 		//7.1 - CRIPTA
@@ -135,59 +117,7 @@ public class WriteLayout implements GeneralLayout{
 		c.ipadx= 0;c.ipady= 0;
 		button.setBackground(Color.BLUE);
 		button.setForeground(Color.WHITE);
-		button.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				if(receiverSetted){
-					String selected_text= area.getSelectedText();
-					int cursor_position= area.getCaretPosition() - selected_text.length();
-					System.out.println(selected_text);
-					selected_text= cursor_position + "_" + selected_text;
-					System.out.println(selected_text);
-					System.out.println("La posizione del cursore nel testo è: " + cursor_position);
-					if(selected_text != null){
-						int start= area.getSelectionStart();
-						int end= area.getSelectionEnd();
-						int text_lenght= end - start;
-						System.out.println("text_lengt = " + text_lenght);
-
-						String txt= area.getText();
-						String left_part= txt.substring(0, start);
-						String right_part= txt.substring(end);
-						String middle_part= "##########";
-
-						setAreaText(left_part + middle_part + right_part); 
-
-						try {
-							byte[] encripted= CryptoUtility.encrypt(CryptoUtility.CRYPTO_ALGO.AES, selected_text, "giovannino");
-							String enc= new String(encripted);
-							System.out.println("lunghezza criptata = " + enc.length());
-							System.out.println("Selezione criptata: " + enc);
-
-							QRCode qr = new QRCode();
-							qr.writeQRCode(enc, QRCode.DEFAULT_WIDTH, QRCode.DEFAULT_HEIGHT);
-							configurePath();
-							//--------------------------------------------------------------
-							//String qrcode_file= getOutput_folder()+"/"+getNameFile()+".jpg";
-							String qrcode_file= "/home/pasquale/ProgettoSicurezza"+"/"+getNameFile()+".jpg";
-							int i=1;
-							while(checkExists(qrcode_file)){
-								qrcode_file= qrcode_file.substring(0, qrcode_file.lastIndexOf("_")+1);
-								qrcode_file= qrcode_file + i + ".jpg";
-								i++;
-							}
-							qr.saveQRCodeToFile(qrcode_file);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}
-				else{
-					JOptionPane.showMessageDialog(pane, "Prima selezionare il destinatario", "attenzione", JOptionPane.INFORMATION_MESSAGE);
-				}
-			}
-		});
+		button.addActionListener(new EncodeAction());
 		pane.add(button, c);
 		//7.2 - NUOVO
 		button= new JButton("NUOVO");
@@ -195,28 +125,7 @@ public class WriteLayout implements GeneralLayout{
 		c.gridx= posx;c.gridy= posy;
 		button.setBackground(Color.BLUE);
 		button.setForeground(Color.WHITE);
-		button.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				area.setText("");
-				System.out.println("cancellato contenuto JTextArea");
-
-				JFileChooser file_chooser= new JFileChooser();
-				file_chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				int choose= file_chooser.showDialog(null, "SELEZIONA");
-				if(choose == JFileChooser.APPROVE_OPTION){
-					String name_file= JOptionPane.showInputDialog(getPane(), "inserisci il nome del file\nsenza estensione");
-					setOutput_folder(file_chooser.getSelectedFile().getAbsolutePath()+"/");
-					setNameFile(name_file+"_0");
-					System.out.println("Il percorso scelto è: " + getOutput_folder());
-					
-				}
-				else if(choose == JFileChooser.CANCEL_OPTION){
-					System.out.println("annullato");
-				}
-			}
-		});
+		button.addActionListener(new NewAction());
 		pane.add(button, c);
 		//7.3 - SALVA
 		button= new JButton("SALVA");
@@ -224,43 +133,7 @@ public class WriteLayout implements GeneralLayout{
 		c.gridx= posx;c.gridy= posy;
 		button.setBackground(Color.BLUE);
 		button.setForeground(Color.WHITE);
-		button.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-
-				JFileChooser file_chooser= new JFileChooser(getOutput_folder());
-				file_chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				int choose= file_chooser.showDialog(null, "SELEZIONA");
-				String path="";
-				if(choose == JFileChooser.APPROVE_OPTION){
-					String name_file= JOptionPane.showInputDialog(getPane(), "inserisci il nome del file\nsenza estensione", getNameFile());
-					setOutput_folder(file_chooser.getSelectedFile().getAbsolutePath() +  "/");
-					File f= new File(path);
-					f.mkdir();
-					path= getOutput_folder() + name_file +  ".txt";
-					System.out.println("Il percorso scelto è: " + path);
-					QRCode.saveFile(path, area.getText());//funzione di utilità sta in QRCode solo per comodità
-					try {
-						String pat_pdf= getOutput_folder() + name_file + ".pdf";
-						PDFUtil.create(pat_pdf);
-						if(PDFUtil.open()){
-							PDFUtil.addText(area.getText(), 40, 400, 0);
-							PDFUtil.close();
-						}
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-					} catch (DocumentException e) {
-						e.printStackTrace();
-					}
-					System.out.println("File salvato in: " + path);
-					
-				}
-				else if(choose == JFileChooser.CANCEL_OPTION){
-					System.out.println("annullato");
-				}
-			}
-		});
+		button.addActionListener(new SaveAction());
 		pane.add(button, c);
 		
 	}
@@ -387,4 +260,144 @@ public class WriteLayout implements GeneralLayout{
 	public void setReceiverSetted(boolean receiverSetted) {
 		this.receiverSetted = receiverSetted;
 	}
+	
+	private class SelectReceiverAction implements ActionListener{
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String[] options= {"Utente specifico", "Gruppo di utenti"};
+			int choice= JOptionPane.showOptionDialog(pane, "Come criptare il documento", "Attenzione", JOptionPane.YES_NO_OPTION, 
+					JOptionPane.QUESTION_MESSAGE, null, options, "Seleziona");
+			//utente specifico
+			if(choice == JOptionPane.YES_OPTION){
+				setReceiverSetted(true);
+				control.setLayout("RECEIVER");
+				control.setReceiverSingleUSer(true);
+			}
+			//gruppo di utenti
+			else if(choice == JOptionPane.NO_OPTION){
+				setReceiverSetted(true);
+				control.setLayout("RECEIVER");
+				control.setReceiverSingleUSer(false);
+			}
+		}
+	}
+
+	private class EncodeAction implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			if(receiverSetted){
+				String selected_text= area.getSelectedText();
+				int cursor_position= area.getCaretPosition() - selected_text.length();
+				System.out.println(selected_text);
+				selected_text= cursor_position + "_" + selected_text;
+				System.out.println(selected_text);
+				System.out.println("La posizione del cursore nel testo è: " + cursor_position);
+				if(selected_text != null){
+					int start= area.getSelectionStart();
+					int end= area.getSelectionEnd();
+					int text_lenght= end - start;
+					System.out.println("text_lengt = " + text_lenght);
+
+					String txt= area.getText();
+					String left_part= txt.substring(0, start);
+					String right_part= txt.substring(end);
+					String middle_part= "##########";
+
+					setAreaText(left_part + middle_part + right_part); 
+
+					try {
+						//TODO prendere la chiave pubblica per criptare dal controller
+						byte[] encripted= CryptoUtility.encrypt(CryptoUtility.CRYPTO_ALGO.AES, selected_text, "giovannino");
+						String enc= new String(encripted);
+						System.out.println("lunghezza criptata = " + enc.length());
+						System.out.println("Selezione criptata: " + enc);
+
+						QRCode qr = new QRCode();
+						qr.writeQRCode(enc, QRCode.DEFAULT_WIDTH, QRCode.DEFAULT_HEIGHT);
+						configurePath();
+						//--------------------------------------------------------------
+						//String qrcode_file= getOutput_folder()+"/"+getNameFile()+".jpg";
+						//TODO prendere il percorso per salvare dalle preferenze dell'utente
+						String qrcode_file= "/home/pasquale/ProgettoSicurezza"+"/"+getNameFile()+".jpg";
+						int i=1;
+						while(checkExists(qrcode_file)){
+							qrcode_file= qrcode_file.substring(0, qrcode_file.lastIndexOf("_")+1);
+							qrcode_file= qrcode_file + i + ".jpg";
+							i++;
+						}
+						qr.saveQRCodeToFile(qrcode_file);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			else{
+				JOptionPane.showMessageDialog(pane, "Prima selezionare il destinatario", "attenzione", JOptionPane.INFORMATION_MESSAGE);
+			}
+		}
+	}
+
+	private class NewAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			area.setText("");
+			System.out.println("cancellato contenuto JTextArea");
+
+			JFileChooser file_chooser= new JFileChooser();
+			file_chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+			int choose= file_chooser.showDialog(null, "SELEZIONA");
+			if(choose == JFileChooser.APPROVE_OPTION){
+				String name_file= JOptionPane.showInputDialog(getPane(), "inserisci il nome del file\nsenza estensione");
+				setOutput_folder(file_chooser.getSelectedFile().getAbsolutePath()+"/");
+				setNameFile(name_file+"_0");
+				System.out.println("Il percorso scelto è: " + getOutput_folder());
+
+			}
+			else if(choose == JFileChooser.CANCEL_OPTION){
+				System.out.println("annullato");
+			}
+		}
+	}
+
+	private class SaveAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+
+			JFileChooser file_chooser= new JFileChooser(getOutput_folder());
+			file_chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			int choose= file_chooser.showDialog(null, "SELEZIONA");
+			String path="";
+			if(choose == JFileChooser.APPROVE_OPTION){
+				String name_file= JOptionPane.showInputDialog(getPane(), "inserisci il nome del file\nsenza estensione", getNameFile());
+				setOutput_folder(file_chooser.getSelectedFile().getAbsolutePath() +  "/");
+				File f= new File(path);
+				f.mkdir();
+				path= getOutput_folder() + name_file +  ".txt";
+				System.out.println("Il percorso scelto è: " + path);
+				QRCode.saveFile(path, area.getText());//funzione di utilità sta in QRCode solo per comodità
+				//TODO creare il pdf con i dati
+				try {
+					String pat_pdf= getOutput_folder() + name_file + ".pdf";
+					PDFUtil.create(pat_pdf);
+					if(PDFUtil.open()){
+						PDFUtil.addText(area.getText(), 40, 400, 0);
+						PDFUtil.close();
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (DocumentException e) {
+					e.printStackTrace();
+				}
+				System.out.println("File salvato in: " + path);
+
+			}
+			else if(choose == JFileChooser.CANCEL_OPTION){
+				System.out.println("annullato");
+			}
+		}
+	}
+		
 }
--- ProgettoSicurezzaV0.0/src/test/TestPDF.java
+++ ProgettoSicurezzaV0.0/src/test/TestPDF.java
@@ -32,18 +32,33 @@ public class TestPDF {
 			//PDFUtil.addCredentials("test_pdf", "prova", "java", "PV", "PV");
 
 			String title= "title";
-			PDFUtil.addLogo(PDFUtil.LOGO_PATH);
+			String author= "author";
+			String text= "text";
+			String[] subtitleInfo= {"date", "pagenumber", "info", "receiver"};
+			String[] qrCodes= {"/home/pasquale/ProgettoSicurezza/test_0.jpg", 
+					"/home/pasquale/ProgettoSicurezza/test_1.jpg", 
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg",
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg",
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg",
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg",
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg",
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg",
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg",
+					"/home/pasquale/ProgettoSicurezza/test_0.jpg"};
+			String signaturePath= "/home/pasquale/ProgettoSicurezza/test_1.jpg";
+			/*PDFUtil.addLogo(PDFUtil.LOGO_PATH);
 			PDFUtil.addTitle(title);
 			PDFUtil.addAuthor("author");
 			PDFUtil.addSubtitleInfo("date", "1/1", "info", "receiver");
 			PDFUtil.addLineHorizontal(10, 150, 0);
 			PDFUtil.addLineHorizontal(10, 600, 0);
-			PDFUtil.addImage("/home/pasquale/ProgettoSicurezza/test_0_101.jpg", 10, 610);
-			PDFUtil.addImage("/home/pasquale/ProgettoSicurezza/test_0_200.jpg", 120, 610);
-			PDFUtil.addImage("/home/pasquale/ProgettoSicurezza/test_0_297.jpg", 230, 610);
-			PDFUtil.addImage("/home/pasquale/ProgettoSicurezza/test_0_393.jpg", 340, 610);
-			PDFUtil.addImage("/home/pasquale/ProgettoSicurezza/test_0_484.jpg", 450, 610);
-			
+			PDFUtil.addRectangle(10, 710, 100, 100);*/
+			//PDFUtil.addQRCodeImage("/home/pasquale/ProgettoSicurezza/test_0_101.jpg", 10, 610);
+			//PDFUtil.addQRCodeImage("/home/pasquale/ProgettoSicurezza/test_0_200.jpg", 120, 610);
+			//PDFUtil.addQRCodeImage("/home/pasquale/ProgettoSicurezza/test_0_297.jpg", 230, 610);
+			//PDFUtil.addQRCodeImage("/home/pasquale/ProgettoSicurezza/test_0_393.jpg", 340, 610);
+			//PDFUtil.addQRCodeImage("/home/pasquale/ProgettoSicurezza/test_0_484.jpg", 450, 610);
+			PDFUtil.createDocument(title, author, text, signaturePath, subtitleInfo, qrCodes);
 			
 			
 			//PDFUtil.addLineVertical(40, 30, 400);
